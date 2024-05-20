5a89c7906bee0cd503a8b306d4ecc72418646d32
nevest <guilhermejcarlucci@hotmail.com>
2024-05-19 23:14:16 -0300

    fix ts erros

--- index.ts
+++ index.ts
@@ -1,27 +1,45 @@
+// const { Client, Collection, Events } = require('discord.js');
 import { Client, Collection, GatewayIntentBits, Events } from 'discord.js';
 import 'dotenv/config';
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const { TOKEN } = process.env;
 
+// Definição da interface CustomClient extendendo Client
 interface CustomClient extends Client {
   commands: Collection<string, any>;
+  login(token?: string): Promise<string>;
+  once(event: string | symbol, listener: (...args: any[]) => void): this;
+  on(event: string | symbol, listener: (...args: any[]) => void): this;
 }
 
 // Estender a classe Client
 class MyClient extends Client implements CustomClient {
   commands: Collection<string, any>;
-
+  
   constructor() {
     super({
       intents: [],
     });
     this.commands = new Collection();
   }
+
+  // Implementação dos métodos da interface CustomClient
+  login(token?: string): Promise<string> {
+    return super.login(token);
+  }
+
+  once(event: string | symbol, listener: (...args: any[]) => void): this {
+    return super.once(event, listener);
+  }
+
+  on(event: string | symbol, listener: (...args: any[]) => void): this {
+    return super.on(event, listener);
+  }
 }
 
-const client = new MyClient() as CustomClient;
+const client = new MyClient();
 
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
@@ -36,13 +54,13 @@ for (const file of commandFiles) {
     }
 }
 
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c: any) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(TOKEN);
 
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction: any) => {
     if (!interaction.isChatInputCommand()) return;
     const command:any = client.commands.get(interaction.commandName);
     if(!command) {
@@ -56,3 +74,4 @@ client.on(Events.InteractionCreate, async interaction => {
         await interaction.reply('Got an error.');
     }
 });
+
