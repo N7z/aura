88bf63af08d50bc89dc473e05fa5804e4dafa9ee
nevest <guilhermejcarlucci@hotmail.com>
2024-08-04 19:25:16 -0300

    chat nuker

---
+++ commands/chatNuker.ts
@@ -0,0 +1,31 @@
+import { TextChannel, SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+   data: new SlashCommandBuilder()
+        .setName('nuke')
+        .setDescription('Nuka o chat atual, recriando-o com as mesmas permissões.'),
+        
+    async execute(interaction: any) {
+        if (!interaction.guild) return;
+
+    const channel = interaction.channel;
+    if (!(channel instanceof TextChannel)) {
+        await interaction.reply('Este comando só pode ser usado em canais de texto.');
+        return;
+    }
+
+    try {
+        const channelPosition = channel.position;
+        const newChannel = await channel.clone();
+
+        await channel.delete('Nuked the channel');
+        await newChannel.setPosition(channelPosition);
+        await newChannel.send('Este canal foi nukado!');
+
+        await interaction.reply('O canal foi nukado com sucesso!');
+    } catch (error) {
+            console.error('Erro ao nukar o canal:', error);
+            await interaction.reply('Ocorreu um erro ao tentar nukar o canal.');
+        }
+    }
+};
