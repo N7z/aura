ad3c4a7b25ccecab57335735340f10ed2d59a1ac
nevest <guilhermejcarlucci@hotmail.com>
2024-08-19 00:50:32 -0300

    profile scrap any user

--- commands/profilleScrapper.ts
+++ commands/profilleScrapper.ts
@@ -1,29 +1,30 @@
-const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile-scrapper')
         .setDescription('Pega a foto de perfil de um membro.')
-        .addUserOption((option:any) =>
-            option.setName('membro')
-                .setDescription('Mencione o membro, exemplo: @membro')
+        .addStringOption((option: any) =>
+            option.setName('user_id')
+                .setDescription('ID do usuário ou @membro')
                 .setRequired(true)),
-        async execute(interaction:any) {
-            const member = interaction.options.getMember('membro');
+    async execute(interaction: any) {
+        const userId = interaction.options.getString('user_id');
 
-            if (!member) {
-                await interaction.reply('Você deve fornecer um membro válido.');
-                return;
-            }
-
-            const avatarURL = member.user.displayAvatarURL({ dynamic: true });
+        try {
+            const user = await interaction.client.users.fetch(userId);
+            const avatarURL = user.displayAvatarURL({ dynamic: true });
 
             const embed = new EmbedBuilder()
                 .setColor('563AF5')
                 .setTitle('Foto de Perfil')
-                .setDescription(`Aqui está a foto de perfil de ${member.user.tag}:`)
+                .setDescription(`Aqui está a foto de perfil de ${user.tag}:`)
                 .setImage(avatarURL);
-            
-            await interaction.reply({ embeds: [embed] }); 
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply('Não foi possível encontrar o usuário. Verifique se o ID está correto.');
         }
-}
+    }
+};
