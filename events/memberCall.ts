c637de03eeee1a526ce4081e2393f733150ab6a6
nevest <guilhermejcarlucci@hotmail.com>
2024-08-26 02:09:40 -0300

    fix call state change, America/Sao_Paulo format hour

--- events/memberCall.ts
+++ events/memberCall.ts
@@ -1,28 +1,37 @@
 import { Events, VoiceState, EmbedBuilder, TextChannel } from "discord.js";
+import { format } from 'date-fns';
+import { toDate } from 'date-fns-tz';
 
 module.exports = {
   name: Events.VoiceStateUpdate,
   async execute(oldState: VoiceState, newState: VoiceState) {
+    const timeZone = 'America/Sao_Paulo';
+    const logChannelId = '1250542002597269565'; // Substitua pelo ID do seu canal de log
+
+    const logChannel = oldState.guild.channels.cache.get(logChannelId) as TextChannel || newState.guild.channels.cache.get(logChannelId) as TextChannel;
+
+    if (!logChannel) {
+      console.error(`Channel com id ${logChannelId} não encontrado.`);
+      return;
+    }
+
+    const now = new Date();
+    const zonedDate = toDate(now, { timeZone });
+    const formattedTime = format(zonedDate, 'HH:mm:ss');
+
+    // Usuário entrou em um canal de voz
     if (!oldState.channel && newState.channel) {
       const member = newState.member;
       const channel = newState.channel;
 
       if (!member) return;
 
-      const logChannelId = '1250542002597269565'; // Substitua pelo ID do seu canal de log
-      const logChannel = member.guild.channels.cache.get(logChannelId) as TextChannel;
-
-      if (!logChannel) {
-        console.error(`Channel with id ${logChannelId} not found.`);
-        return;
-      }
-
       const embed = new EmbedBuilder()
         .setTitle('Entrou em um canal de voz')
         .setThumbnail(member.user.displayAvatarURL())
         .addFields(
           { name: 'Usuário', value: member.user.tag, inline: true },
-          { name: 'Horário', value: new Date().toLocaleTimeString(), inline: true },
+          { name: 'Horário', value: formattedTime, inline: true },
           { name: 'Call', value: channel.name, inline: true }
         )
         .setColor(0x0099ff)
@@ -30,32 +39,24 @@ module.exports = {
 
       try {
         await logChannel.send({ embeds: [embed] });
-        console.log(`Embed enviada para o canal de log ${logChannel.name}.`);
       } catch (e) {
         console.error(e);
       }
     }
 
+    // Usuário saiu de um canal de voz
     if (oldState.channel && !newState.channel) {
       const member = oldState.member;
       const channel = oldState.channel;
 
       if (!member) return;
 
-      const logChannelId = '1250542002597269565'; // Substitua pelo ID do seu canal de log
-      const logChannel = member.guild.channels.cache.get(logChannelId) as TextChannel;
-
-      if (!logChannel) {
-        console.error(`Channel with id ${logChannelId} not found.`);
-        return;
-      }
-
       const embed = new EmbedBuilder()
         .setTitle('Saiu de um canal de voz')
         .setThumbnail(member.user.displayAvatarURL())
         .addFields(
           { name: 'Usuário', value: member.user.tag, inline: true },
-          { name: 'Horário', value: new Date().toLocaleTimeString(), inline: true },
+          { name: 'Horário', value: formattedTime, inline: true },
           { name: 'Call', value: channel.name, inline: true }
         )
         .setColor(0xff0000)
@@ -63,7 +64,33 @@ module.exports = {
 
       try {
         await logChannel.send({ embeds: [embed] });
-        console.log(`Embed enviada para o canal de log ${logChannel.name}.`);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    // Usuário trocou de canal de voz
+    if (oldState.channel && newState.channel && oldState.channel.id !== newState.channel.id) {
+      const member = newState.member;
+      const oldChannel = oldState.channel;
+      const newChannel = newState.channel;
+
+      if (!member) return;
+
+      const embed = new EmbedBuilder()
+        .setTitle('Trocou de canal de voz')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addFields(
+          { name: 'Usuário', value: member.user.tag, inline: true },
+          { name: 'Horário', value: formattedTime, inline: true },
+          { name: 'De', value: oldChannel.name, inline: true },
+          { name: 'Para', value: newChannel.name, inline: true }
+        )
+        .setColor(0xffff00)
+        .setTimestamp();
+
+      try {
+        await logChannel.send({ embeds: [embed] });
       } catch (e) {
         console.error(e);
       }
